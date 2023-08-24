bc24634220009775eae3f943625ec3e8ac0e782e
KevinRhode <kevinrhode@hotmail.com>
2023-08-24 00:06:44 -0400

    add: added user and graphQL for typeDefs and resolvers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import Home from './pages/Home';
 import NoMatch from './pages/NoMatch';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 const router = createBrowserRouter([
   {
@@ -16,13 +18,16 @@ const router = createBrowserRouter([
         index: true, 
         element: <Home />,
       }, 
-    //   {
-    //     path: '/login',
-    //     element: <Login />
-    //   }, {
-    //     path: '/signup',
-    //     element: <Signup />
-    //   }, {
+      {
+        path: '/login',
+        element: <Login />,
+        errorElement: <NoMatch />
+      }, {
+        path: '/signup',
+        element: <Signup />,
+        errorElement: <NoMatch />
+      },
+      //  {
     //     path: '/success',
     //     element: <Success />
     //   }, {
